aabd277614bbac1f331f1b38623ea7db2cc4cd2c
Pau Codina <pau.codina@kaldeera.com>
2014-10-30 10:53:10 +0100

    No default expand properties in SPList

--- src/sharepoint/services/splist.js
+++ src/sharepoint/services/splist.js
@@ -123,19 +123,6 @@ angular.module('ngSharePoint').factory('SPList',
 			var self = this;
 			var def = $q.defer();
 			var executor = new SP.RequestExecutor(self.web.url);
-			var defaultExpandProperties = 'Views';
-			// NOTA: Se ha eliminado la expansión automática del objeto 'Forms' debido a 
-			// que si la lista es la 'SiteUserInfoList' se genera un error porque no 
-			// tiene formularios sino que se utiliza la página /_layouts/15/UserDisp.aspx
-			// para visualizar un usuario y un popup para la edición.
-
-			if (query) {
-				query.$expand = defaultExpandProperties + (query.$expand ? ', ' + query.$expand : '');
-			} else {
-				query = { 
-					$expand: defaultExpandProperties
-				};
-			}
 
 			executor.executeAsync({
 
