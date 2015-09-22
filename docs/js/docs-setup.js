3b43ca289c77f2db8402392a89e7b23b7cb21781
Pau Codina <pau.codina@kaldeera.com>
2015-09-22 18:51:23 +0200

    Documentation site

---
+++ docs/js/docs-setup.js
@@ -0,0 +1,70 @@
+NG_DOCS={
+  "sections": {
+    "api": "API Reference"
+  },
+  "pages": [
+    {
+      "section": "api",
+      "id": "ngSharePoint",
+      "shortName": "ngSharePoint",
+      "type": "overview",
+      "moduleName": "ngSharePoint",
+      "shortDescription": "ngSharePoint (core module)",
+      "keywords": "access add aims allows angular api app aspx castro cm codina copyright dependency directives document elemements facilitate forms https include introduction js libraries license licensed lists ll microsoft min mit module myapp ngsharepoint overview pau pedro powerfull rest script services set sharepoint src templates usage users var wrapper"
+    },
+    {
+      "section": "api",
+      "id": "ngSharePoint.SharePoint",
+      "shortName": "ngSharePoint.SharePoint",
+      "type": "object",
+      "moduleName": "ngSharePoint",
+      "shortDescription": "Provides top level access to SharePoint web sites api.",
+      "keywords": "access allows api context current executed function getcurrentweb getweb initialized level methods ngsharepoint object promise properties required retrieve returns rrhh sentence sharepoint sites sputils spweb top url web"
+    },
+    {
+      "section": "api",
+      "id": "ngSharePoint.SPList",
+      "shortName": "ngSharePoint.SPList",
+      "type": "object",
+      "moduleName": "ngSharePoint",
+      "shortDescription": "Represents a SPList object that you could use to access to all SharePoint list properties and data.",
+      "keywords": "access accomplish additional alert allowcontenttypes api aspx attach based basetemplate bk_fieldcalculated bk_fieldcollection bk_fieldproperties bk_listproperties call called choice choices closed collection collects complete complex console construct consult contenttypes create creates current data default defaultvalue defined definition definitions direct directly docs documents doesn draft duedate editor effectivebasepermissions enableattachments example executed expressions extend extended false field fieldcalculated fieldchoice fieldcollection fieldcomputed fieldcurrency fielddatetime fieldgeolocation fieldguid fieldlookup fieldmultichoice fieldmultilinetext fieldnumber fieldratingscale fields fieldtext fieldurl fielduser file folders follow forcecheckout forms function future getcurrentweb getfields getlist getproperties https implemented initialize initialized instance instances instantiates interfaces items list listid listproperties located log lookup method methods microsoft modified modify moment named ngsharepoint object objects open orders param parameter point points pre-initialized primary projectstatus promise properties property query readonlyfield recives recovered refer reference represents required rest retrieve retrieves returned returns schema server shared sharepoint site specific splist spweb structures system title true type updateproperties userinfolist users valid validation values views web"
+    },
+    {
+      "section": "api",
+      "id": "ngSharePoint.SPUser",
+      "shortName": "ngSharePoint.SPUser",
+      "type": "object",
+      "moduleName": "ngSharePoint",
+      "shortDescription": "Represents a SPUser object that is used to access to all SharePoint user properties",
+      "keywords": "_spcontextinfo _sppagecontextinfo access accomplish allows api aspx bk_userproperties browser call calls care complete complex configured correctly current currentuser data difference direct directly documentation evirontment executed extend extended function getproperties groups http https implemented initialize instantiate instantiated instantiates issiteadmin list loged login loginname method methods microsoft moment mydomain ngsharepoint object objects order param parameter pointer points prefer primary promise properties query recovered reference represents rest retrieval retrieve returned returns server service sharepoint specific spuser spweb structures title url user userid valid values var variable view web whitch"
+    },
+    {
+      "section": "api",
+      "id": "ngSharePoint.SPWeb",
+      "shortName": "ngSharePoint.SPWeb",
+      "type": "object",
+      "moduleName": "ngSharePoint",
+      "shortDescription": "Represents a SPWeb object that are used to access to all SharePoint web site properties, lists and users.",
+      "keywords": "access accomplish alert angular api array aspx bk_webproperties call calls complete complex configured console content contenttypes correctly current default description desired direct directly document enableattachments example executed extend extended foreach function getcurrentuser getcurrentweb getlist getlistitems getlists getproperties getuser getuserbyid group groups guid http https implemented initialized initiated instance instances instantiate instantiated instantiates issiteadmin language libraries library list lists log method methods microsoft moment musn ngsharepoint object objects param parameter point pointer points primary promise properties provided query recovered refer reference regionalsettings represents rest retrieve retrieves returned returns server service sharepoint site sitegroups specific splist spuser sputils spweb structures stuff system tasklist tasks title url user userid userinfolist users values var web webobject"
+    },
+    {
+      "section": "api",
+      "id": "ngSharePointFormPage",
+      "shortName": "ngSharePointFormPage",
+      "type": "overview",
+      "moduleName": "ngSharePointFormPage",
+      "shortDescription": "Adds &#39;spform&#39; directive and bootstrap the angular application with the correct SharePoint List/Item page context.",
+      "keywords": "adds angular api application bootstrap castro cm codina context copyright correct directive license licensed list mit ngsharepointformpage overview pau pedro sharepoint spform"
+    }
+  ],
+  "apis": {
+    "api": true
+  },
+  "html5Mode": false,
+  "editExample": true,
+  "startPage": "/api/ngSharePoint",
+  "scripts": [
+    "angular.min.js"
+  ]
+};
